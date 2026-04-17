20b999a284a0110f56b78aebdead9d9800d60927
shayo07 <shayody@gmail.com>
2026-04-17 15:20:43 +0400

    changes

---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,17 @@
+import { Link } from 'expo-router';
+import { styled } from "nativewind";
+import React from 'react';
+import { Text } from 'react-native';
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+const SafeAreaView = styled(RNSafeAreaView)
+
+const SignUp = () => {
+  return (
+    <SafeAreaView className="flex-1 bg-background p-5">
+      <Text>SignUp</Text>
+      <Link href="/sign-in" className="mt-4 rounded bg-primary text-white px-4 py-2"> go to sign in </Link>
+    </SafeAreaView>
+  )
+}
+
+export default SignUp
---
+++ constants/data.ts
@@ -0,0 +1,8 @@
+import { icons } from "./icons";
+
+export const tabs = [
+    { name: "index", title: "Home", icon: icons.home },
+    { name: "insights", title: "Insights", icon: icons.activity },
+    { name: "settings", title: "Settings", icon: icons.setting },
+    { name: "subscriptions", title: "Subscriptions", icon: icons.wallet },
+]
