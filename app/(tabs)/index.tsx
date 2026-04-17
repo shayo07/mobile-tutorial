f94bc95ae566d926893100f792f2598bf7463247
shayo07 <104032446+shayo07@users.noreply.github.com>
2026-04-17 15:42:00 +0400

    Update app/(tabs)/index.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,7 +7,7 @@ const SafeAreaView = styled(RNSafeAreaView)
 export default function App() {
  
   return (
-    <SafeAreaView className="flex-1 bg-background, p-5">
+    <SafeAreaView className="flex-1 bg-background p-5">
       <Text className="text-xl font-bold text-success">
         Welcome to Nativewind!
       </Text>
