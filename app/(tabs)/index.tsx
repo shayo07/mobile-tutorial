4f078d8eed2f68f0af2bc2ee42c38321eec24ab4
shayo07 <shayody@gmail.com>
2026-04-17 16:18:07 +0400

    implement fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,14 +8,12 @@ export default function App() {
  
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
-      <Link href="/onboarding" className="mt-4 rounded bg-primary text-white px-4 py-2"> go to onboarding </Link>
-      <Link href="/sign-in" className="mt-4 rounded bg-primary text-white px-4 py-2"> go to sign in </Link>
-      <Link href="/sign-up" className="mt-4 rounded bg-primary text-white px-4 py-2"> go to sign up </Link>
-      <Link href="/subscriptions/spotify" className="mt-4 rounded bg-primary text-white px-4 py-2"> go to subscription details </Link>
-      <Link href={{ pathname: "/subscriptions/[id]", params: { id: "spotify" } }} className="mt-4 rounded bg-primary text-white px-4 py-2"> go to subscription details with params </Link>
+      <Text className="text-5xl font-sans-extrabold text-success">Welcome!</Text>
+      <Link href="/onboarding" className="mt-4 font-sans-bold rounded bg-primary text-white px-4 py-2"> go to onboarding </Link>
+      <Link href="/sign-in" className="mt-4 font-sans-bold rounded bg-primary text-white px-4 py-2"> go to sign in </Link>
+      <Link href="/sign-up" className="mt-4 font-sans-bold rounded bg-primary text-white px-4 py-2"> go to sign up </Link>
+      <Link href="/subscriptions/spotify" className="mt-4 font-sans-bold rounded bg-primary text-white px-4 py-2"> go to subscription details </Link>
+      <Link href={{ pathname: "/subscriptions/[id]", params: { id: "spotify" } }} className="mt-4 font-sans-bold rounded bg-primary text-white px-4 py-2"> go to subscription details with params </Link>
     </SafeAreaView>
   );
 }
